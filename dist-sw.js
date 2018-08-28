4116942fc70d1e1e1002d8a02a903e07cc923088
justinandjesus <justinandjesus@gmail.com>
2018-08-28 14:54:53 +0800

    Updates

---
+++ dist-sw.js
@@ -0,0 +1,18 @@
+importScripts("/precache-manifest.552612b125304f3ca212aeb33e1ffbf7.js", "https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+
+// importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+
+workbox.routing.registerRoute(
+    new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+    workbox.strategies.cacheFirst({
+      cacheName: 'google-fonts',
+      plugins: [
+        new workbox.cacheableResponse.Plugin({
+          statuses: [0, 200],
+        }),
+      ],
+    }),
+  );
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
---
+++ precache-manifest.552612b125304f3ca212aeb33e1ffbf7.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "e05b5c77307dc33fc5513fdadd43c215",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd8a8a172eed2b61136b",
+    "url": "/bundle.js"
+  },
+  {
+    "revision": "323f9ff923ff4a088761604e8cc49ea8",
+    "url": "/audio/add.mp3"
+  }
+];
